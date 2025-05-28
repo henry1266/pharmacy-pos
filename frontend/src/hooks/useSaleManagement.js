6cc5a0a85edab5bae39b81303cd8d0bb4c88ed93
zxh1266 <zxh1266@gmail.com>
2025-05-28 16:33:34 +0000

    修改

--- frontend/src/hooks/useSaleManagement.js
+++ frontend/src/hooks/useSaleManagement.js
@@ -159,7 +159,7 @@ const useSaleManagement = (showSnackbar) => {
         paymentMethod: currentSale.paymentMethod,
         paymentStatus: currentSale.paymentStatus,
         note: currentSale.note,
-        // cashier: '...' // TODO: Replace with actual logged-in user ID
+        // TODO: Replace with actual logged-in user ID
       };
       await createSale(saleData);
       showSnackbar('銷售記錄已保存', 'success');
