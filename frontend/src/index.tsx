fe187c0e0b0a659706162fc6142aad87edba707e
zxh1266 <zxh1266@gmail.com>
2025-06-19 21:47:30 +0800

    feat: Add main application entry point with React and Redux setup

---
+++ frontend/src/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+// @ts-ignore - 忽略 TypeScript 對 .tsx 副檔名的錯誤
+import App from './App.tsx';
+import './index.css';
+
+// 使用 TypeScript 類型定義
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
