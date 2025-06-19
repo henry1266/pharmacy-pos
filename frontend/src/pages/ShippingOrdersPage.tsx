3082d14133000638c0241d2374e42a8a15575aea
zxh1266 <zxh1266@gmail.com>
2025-06-20 00:21:43 +0800

    refactor: Update import path to include file extension for ShippingOrdersFilter in ShippingOrdersPage

--- frontend/src/pages/ShippingOrdersPage.tsx
+++ frontend/src/pages/ShippingOrdersPage.tsx
@@ -30,7 +30,7 @@ import { fetchShippingOrders } from '../redux/actions';
 import ShippingOrderPreview from '../components/shipping-orders/ShippingOrderPreview.tsx';
 import SupplierCheckboxFilter from '../components/filters/SupplierCheckboxFilter';
 import ShippingOrdersTable from '../components/shipping-orders/list/ShippingOrdersTable.tsx';
-import ShippingOrdersFilter from '../components/shipping-orders/list/ShippingOrdersFilter';
+import ShippingOrdersFilter from '../components/shipping-orders/list/ShippingOrdersFilter.tsx';
 import CsvImportDialog from '../components/shipping-orders/import/CsvImportDialog';
 import ShippingOrderImportOptions from '../components/shipping-orders/import/ShippingOrderImportOptions';
 import GenericConfirmDialog from '../components/common/GenericConfirmDialog';
